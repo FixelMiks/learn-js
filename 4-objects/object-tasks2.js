9425e549d47682a9ba0710c6b64a3962618a5418
Samat Malkvaev <malkvaevsamat@gmail.com>
2024-02-27 21:10:47 +0600

    learn more code

--- 4-objects/object-tasks2.js
+++ 4-objects/object-tasks2.js
@@ -74,3 +74,87 @@ console.log(anotherObj); // {[object Object]: 123}
 console.log(typeof anotherObj[obj4]); // number
 console.log(Boolean(anotherObj)); // true
 //
+const obj5 = {};
+
+let objectToNumber = +obj5;
+
+console.log(typeof objectToNumber); // number
+console.log(objectToNumber); // NaN
+//
+const obj6 = {};
+
+let objectToString = obj6.toString();
+
+console.log(typeof objectToString); // string
+console.log(objectToString); // [object Object]
+//
+const objNum1 = {};
+const objNum2 = {};
+console.log(objNum1 > objNum2); // false
+console.log(objNum1 < objNum2); // false
+console.log(objNum1 == objNum2); // false
+console.log(objNum1 === objNum2); // false
+console.log(objNum1 <= objNum2); // true
+console.log(objNum1 >= objNum2); // true
+//
+let user = {
+  name: "John",
+  money: 1000,
+
+  [Symbol.toPrimitive](hint) {
+    alert(`hint: ${hint}`);
+    return hint == "string" ? `{name: "${this.name}"}` : this.money;
+  },
+};
+
+alert(user); // hint: string -> {name: "John"}
+alert(+user); // hint: number -> 1000
+alert(user + 500); // hint: default -> 1500
+//
+let user1 = {
+  name: "John",
+  money: 1000,
+
+  // для хинта равного "string"
+  toString() {
+    return `{name: "${this.name}"}`;
+  },
+
+  // для хинта равного "number" или "default"
+  valueOf() {
+    return this.money;
+  },
+};
+
+alert(user1); // toString -> {name: "John"}
+alert(+user1); // valueOf -> 1000
+alert(user1 + 500); // valueOf -> 1500
+//
+let objEqualsString = {
+  toString() {
+    return "2";
+  },
+};
+
+console.log(objEqualsString == "2"); // true
+//
+let objEqualsNum = {
+  valueOf() {
+    return 2;
+  },
+};
+
+console.log(objEqualsNum == 2); // true
+//
+let objMoreZero = {
+  valueOf() {
+    return 2;
+  },
+};
+
+console.log(objEqualsNum > 0); // true
+//
+{
+}
++[] + {} + [1]; // '0[object Object]1'
+console.log({} + [] + {} + [1]); // [object Object][object Object]1
