616f409928a7de0dc405fb354042774f08f9f686
Samat Malkvaev <malkvaevsamat@gmail.com>
2024-08-08 20:04:45 +0600

    added code from live coding

--- 6-advanced-functions/advanced-functions-tasks-3.js
+++ 6-advanced-functions/advanced-functions-tasks-3.js
@@ -75,3 +75,45 @@ let sayHi = function (who) {
 };
 
 sayHi();
+
+// Live Coding
+// task 1
+function DNAStrand(dna) {
+  // console.log(dna);
+  let dnaArr = dna.split("");
+  // console.log(dnaArr);
+  let outputDna = "";
+  for (letter of dnaArr) {
+    // console.log(letter);
+    if (letter === "A") {
+      outputDna += "T";
+    } else if (letter === "T") {
+      outputDna += "A";
+    } else if (letter === "G") {
+      outputDna += "C";
+    } else if (letter === "C") {
+      outputDna += "G";
+    }
+  }
+  return outputDna;
+}
+
+DNAStrand("ATTGC");
+DNAStrand("GTAT");
+
+// task 2
+function explode(x) {
+  const filterX = x.filter((el) => typeof el === "number");
+  // console.log(filterX);
+  if (filterX.length === 1) {
+    return new Array(filterX[0]).fill(x);
+  } else if (filterX.length === 2) {
+    return new Array(filterX[0] + filterX[1]).fill(x);
+  } else {
+    return "Void!";
+  }
+}
+
+console.log(explode(["a", 3]));
+console.log(explode([9, 3]));
+console.log(explode(["a", "b"]));
